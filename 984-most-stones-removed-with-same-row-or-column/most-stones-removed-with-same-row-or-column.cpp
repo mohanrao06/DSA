02d25eb116fb8a447cedaa64790200f7d56ca2a8
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2025-10-21 16:45:27 +0530

    Time: 17 ms (64.99%) | Memory: 25.6 MB (15.61%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,31 +1,77 @@
+class DisjointSet{
+    private:
+        vector<int> parent,rank,size;
+    public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+
+    int findParent(int node){
+        if(node==parent[node])return node;
+        return parent[node]=findParent(parent[node]);
+
+    }
+
+    void unionByRank(int u,int v){
+        int pu=findParent(u);
+        int pv=findParent(v);
+        if(pu==pv)return;
+        if(rank[pu]<rank[pv]){
+            parent[pu]=pv;
+        }
+        else if(rank[pv]<rank[pu]){
+            parent[pv]=pu;
+        }
+        else{
+            parent[pu]=pv;
+            rank[pu]++;
+        }
+    }
+    void unionByize(int u,int v){
+        int pu=findParent(u);
+        int pv=findParent(v);
+        if(pu==pv)return;
+        if(size[pu]<size[pv]){
+            parent[pu]=pv;
+            size[pv]+=size[pu];
+        }
+        else{
+            parent[pv]=pu;
+            size[pu]+=size[pv];
+        }
+    }
+};
 class Solution {
-private:
-    int connectedComponentCount = 0;
-
-    int findRepresentative(int element, vector<int>& setRepresentatives) {
-        if (setRepresentatives[element] == 0) {
-            setRepresentatives[element] = element;
-            connectedComponentCount++;
-        }
-        return setRepresentatives[element] == element ? element : 
-               (setRepresentatives[element] = findRepresentative(setRepresentatives[element], setRepresentatives));
-    }
-
-    void mergeComponents(int elementA, int elementB, vector<int>& setRepresentatives) {
-        int repA = findRepresentative(elementA, setRepresentatives);
-        int repB = findRepresentative(elementB, setRepresentatives);
-        if (repA != repB) {
-            setRepresentatives[repB] = repA;
-            connectedComponentCount--;
-        }
-    }
-
 public:
-    int removeStones(vector<vector<int>>& stonePositions) {
-        vector<int> setRepresentatives(20003, 0);
-        for (const auto& stonePosition : stonePositions) {
-            mergeComponents(stonePosition[0] + 1, stonePosition[1] + 10002, setRepresentatives);
+    int removeStones(vector<vector<int>>& stones) {
+        int maxrows=0;
+        int maxcol=0;
+        for(auto it:stones){
+            maxrows=max(maxrows,it[0]);
+            maxcol=max(maxcol,it[1]);
         }
-        return stonePositions.size() - connectedComponentCount;
+        DisjointSet ds(maxrows+maxcol+1);
+        unordered_map<int,int> mp;
+
+
+        for(auto it:stones){
+            int u=it[0];
+            int v=it[1]+maxrows+1;
+            ds.unionByize(u,v);
+            mp[u]=1;
+            mp[v]=1;
+
+        }
+        int cnt=0;
+        for(auto it:mp){
+            if(ds.findParent(it.first)==it.first)cnt++;
+        }
+        return stones.size()-cnt;
+
     }
 };
