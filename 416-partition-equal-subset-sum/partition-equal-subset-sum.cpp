32776f74395741db0f89079858fc2c6bbb1a72ee
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2025-11-05 16:37:49 +0530

    Time: 146 ms (39.47%) | Memory: 17.5 MB (60.50%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -19,7 +19,19 @@ public:
         for(int i=0;i<n;i++)sum+=nums[i];
         if(sum%2)return false;
         int target=sum/2;
-        vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
-        return fun(n-1,target,nums,dp);
+        vector<vector<bool>> dp(n+1,vector<bool>(target+1,false));
+        for(int i=0;i<n;i++)dp[i][0]=true;
+         if (nums[0] <= target) dp[0][nums[0]] = true;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+                bool notpick=dp[i-1][j];
+                bool pick=false;
+                if(j>=nums[i]){
+                    pick=dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=pick||notpick;
+            }
+        }
+        return dp[n-1][target];
     }
 };
