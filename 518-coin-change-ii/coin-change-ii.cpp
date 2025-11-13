c5fc24844e6f115115bb5bc54ffb58a9528c3175
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2025-11-13 22:55:21 +0530

    Time: 42 ms (26.09%) | Memory: 61.4 MB (12.02%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -16,9 +16,26 @@ public:
         return dp[ind][t]=nottake+take;
 
     }
-    int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
-        return fun(n-1,amount,coins,dp);
+    int change(int amount, vector<int>& arr) {
+        int n=arr.size();
+        vector<vector<unsigned long long>> dp(n,vector<unsigned long long> (amount+1,0));
+        for(int i=0;i<=amount;i++){
+            if(i%arr[0]==0){
+                dp[0][i]=1;
+            }else{
+                dp[0][i]=0;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                unsigned long long nottake=dp[i-1][j];
+                unsigned long long take=0;
+                if(arr[i]<=j){
+                    take=dp[i][j-arr[i]];
+                }
+                dp[i][j]=take+nottake;
+            }
+        }
+        return (int)dp[n-1][amount];
     }
 };
