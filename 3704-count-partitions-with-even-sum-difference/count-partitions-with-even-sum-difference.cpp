8f3a80fe8e748ca7be794e2e7c8fca0f68e00a82
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2025-12-05 14:34:16 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (53.79%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int countPartitions(vector<int>& nums) {
         int ans=0;
-        int sum1=0;
-        
+        int sum=0;
+        for(int i=0;i<nums.size();i++)sum+=nums[i];
+        int x=0;
         for(int i=0;i<nums.size()-1;i++){
-            sum1+=nums[i];
-            int sum2=0;
-            for(int j=i+1;j<nums.size();j++){
-                sum2+=nums[j];
-            }
-            if((sum1-sum2)%2==0)ans++;
+            x+=nums[i];
+            int y=sum-x;
+            if((x-y)%2==0)ans++;
+
         }
         return ans;
+        
+        
 
     }
 };
