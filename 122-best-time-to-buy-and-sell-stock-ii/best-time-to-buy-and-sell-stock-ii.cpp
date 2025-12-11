da4ad3109641967239b2b7c16a106ee7d72d301a
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2025-12-11 16:55:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (62.47%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
-    int fun(int ind,int buy,vector<int>& prices,vector<vector<int>> &dp){
-        if(ind==prices.size())return 0;
-
-        if(dp[ind][buy]!=-1)return dp[ind][buy];
-        int profit=0;
-        if(buy){
-            profit=max(-prices[ind]+fun(ind+1,0,prices,dp),fun(ind+1,1,prices,dp));
-        }
-        else{
-            profit=max(prices[ind]+fun(ind+1,1,prices,dp),fun(ind+1,0,prices,dp));
-        }
-        return dp[ind][buy]=profit;
-    }
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<long> prev(2,0),curr(2,0);
-        prev[0]=prev[1]=0;
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                int profit=0;
-                if(buy){
-                    profit=max(-prices[ind]+prev[0],prev[1]);
-                }
-                else{
-                    profit=max(prices[ind]+prev[1],prev[0]);
-                }
-                curr[buy]=profit;
+        int ans=0;
+        
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>prices[i-1]){
+                ans=ans+(prices[i]-prices[i-1]);
             }
-            prev=curr;
+            
+
         }
-        return prev[1];
+        return ans;
     }
 };
