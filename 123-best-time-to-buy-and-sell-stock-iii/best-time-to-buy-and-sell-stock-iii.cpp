13197271a6c6ee6e5a27baa34d9151bf75514d6e
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2025-12-12 12:34:12 +0530

    Time: 63 ms (51.78%) | Memory: 79.2 MB (82.66%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -21,41 +21,33 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1, 
-       vector<vector<int>>(2, vector<int>(3, 0)));
-       for(int ind=0;ind<n;ind++){
-            for(int buy=0;buy<=1;buy++){
-                dp[ind][buy][0]=0;
-            }
-       }
-       for(int buy=0;buy<=1;buy++){
-            for(int cap=0;cap<=2;cap++){
-                dp[n][buy][cap]=0;
-            }
-       }
+       vector<vector<int>> after(2, vector<int>(3, 0));
+       vector<vector<int>> curr(2, vector<int>(3, 0));
+    
        for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 for(int cap=1;cap<=2;cap++){
                     int profit=0;
                     if(buy){
                         profit=max(
-                            -prices[ind]+dp[ind+1][0][cap],
-                            dp[ind+1][1][cap]
+                            -prices[ind]+after[0][cap],
+                            after[1][cap]
                         );
                     }else{
                         profit=max(
-                            prices[ind]+dp[ind+1][1][cap-1],
-                            dp[ind+1][0][cap]
+                            prices[ind]+after[1][cap-1],
+                            after[0][cap]
                         );
                     }
-                    dp[ind][buy][cap]=profit;
+                    curr[buy][cap]=profit;
                 }
             }
+            after=curr;
        }
 
 
 
-       return dp[0][1][2];
+       return after[1][2];
 
     }
 };
