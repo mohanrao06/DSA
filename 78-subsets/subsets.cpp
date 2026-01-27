2c54337997a691b5c415b69663af4007ed990c8d
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-01-27 13:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (82.22%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        ans.push_back({}); 
-
-        for(int i = 0; i < nums.size(); i++) {
-            int size = ans.size();
-            for(int j = 0; j < size; j++) {
-                vector<int> temp = ans[j];
-                temp.push_back(nums[i]);
-                ans.push_back(temp);
+        int n=nums.size();
+        for(int mask=0;mask<(1<<n);mask++){
+            vector<int> temp;
+            for(int bit=0;bit<n;bit++){
+                if(mask & (1<<bit)){
+                    temp.push_back(nums[bit]);
+                }
             }
+            ans.push_back(temp);
         }
         return ans;
     }
