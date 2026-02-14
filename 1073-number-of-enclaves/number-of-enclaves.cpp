433460c6fa8d261aafc6678b169918757852fe47
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-02-14 11:49:23 +0530

    Time: 11 ms (39.79%) | Memory: 37.1 MB (32.60%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int drow[4]={-1,0,1,0};
+    int dcol[4]={0,1,0,-1};
+    void dfs(int r,int c,vector<vector<int>>& board,vector<vector<int>> &vis){
+        vis[r][c]=1;
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<4;i++){
+            int nrow=drow[i]+r;
+            int ncol=dcol[i]+c;
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&!vis[nrow][ncol] && board[nrow][ncol]==1){
+                dfs(nrow,ncol,board,vis);
+            }
+        }
+
+
+
+    }
+    int numEnclaves(vector<vector<int>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int col=0;col<m;col++){
+            if(!vis[0][col] && board[0][col]==1){
+                dfs(0,col,board,vis);
+            }
+        }
+        for(int col=0;col<m;col++){
+            if(!vis[n-1][col]&&board[n-1][col]==1){
+                dfs(n-1,col,board,vis);
+            }
+        }
+        for(int row=0;row<n;row++){
+            if(!vis[row][0] &&board[row][0]==1){
+                dfs(row,0,board,vis);
+            }
+        }
+        for(int row=0;row<n;row++){
+            if(!vis[row][m-1] &&board[row][m-1]==1){
+                dfs(row,m-1,board,vis);
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && board[i][j]==1){
+                    cnt++;
+                   
+                }
+            }
+        }
+        return cnt;
+    }
+};
