1305bf54eff23acc27ed63ac5ea4f10854feade0
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-02-16 09:16:16 +0530

    Time: 3 ms (88.88%) | Memory: 35 MB (80.10%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<int,int>> adj[n];
+        vector<vector<pair<int,int>>> adj(n);
         for(int i=0;i<roads.size();i++){
             int u=roads[i][0];
             int v=roads[i][1];
-            int wt=roads[i][2];
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
+            int time=roads[i][2];
+            adj[u].push_back({v,time});
+            adj[v].push_back({u,time});
         }
-        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
-        vector<int> ways(n,0);
         vector<long long> dist(n,LLONG_MAX);
-        dist[0]=0;
+        vector<int> ways(n,0);
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> pq;
         pq.push({0,0});
         ways[0]=1;
-        const int MOD = 1e9 + 7;
+        dist[0]=0;
+        int MOD = 1e9 + 7;
         while(!pq.empty()){
-            long long dis=pq.top().first;
-            int node=pq.top().second;
+            auto node=pq.top();
             pq.pop();
-            for(auto it:adj[node]){
-                int v=it.first;
-                int wt=it.second;
-                long long newdis=wt+dis;
-                if(newdis<dist[v]){
-                    dist[v]=newdis;
-                    pq.push({newdis,v});
-                    ways[v]=ways[node];
-                }
-                else if(newdis==dist[v]){
-                    ways[v]=(ways[v]+ways[node])%MOD;
+            long long d=node.first;
+            int ele=node.second;
+            for(auto it:adj[ele]){
+                int nd=it.first;
+                long long tm=it.second;
+                long long newd=d+tm;
+                if(newd<dist[nd]){
+                    dist[nd]=d+tm;
+                    ways[nd]=ways[ele];
+                    pq.push({d+tm,nd});
+                }else if(d+tm==dist[nd]){
+                    ways[nd]=(ways[nd]+ways[ele])%MOD;
                 }
             }
         }
         return ways[n-1];
+        
+
     }
 };
