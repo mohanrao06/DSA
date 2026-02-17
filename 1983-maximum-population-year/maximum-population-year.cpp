70a19d1b161d3b6912e963f256c05059aa042c8b
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-02-18 00:05:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (8.13%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
         vector<int> arr(2051,0);
+        vector<pair<int,int>> sweep;
         for(int i=0;i<logs.size();i++){
             int st=logs[i][0];
             int end=logs[i][1];
-            arr[st]++;
-            arr[end]--;
+            sweep.push_back({st,+1});
+            sweep.push_back({end,-1});
         }
+        sort(sweep.begin(),sweep.end());
+
         int maxi=0;
         int ans=1950;
         int sum=0;
-        for(int i=1950;i<=2050;i++){
-            sum+=arr[i];
+        for(int i=0;i<sweep.size();i++){
+            sum+=sweep[i].second;
             if(sum>maxi){
                 maxi=sum;
-                ans=i;
+                ans=sweep[i].first;
             }
         }
         return ans;
