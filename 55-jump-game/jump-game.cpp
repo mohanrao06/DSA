b254d24f44ef2ff658b9987dab332a00fa6e4222
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-02-18 12:57:08 +0530

    Time: 174 ms (15.10%) | Memory: 55 MB (7.91%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -13,7 +13,22 @@ public:
     }
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return dfs(nums,0,dp);
+        vector<int> dp(n,0);
+        for(int i=n-1;i>=0;i--){
+            if(i==n-1){
+                dp[i]=1;
+                continue;
+            }
+            for(int j=1;j<=nums[i];j++){
+                if(i+j<n){
+                    if(dp[i+j]){
+                        dp[i]=1;
+                        break;
+                    }
+                }
+            }
+           
+        }
+        return dp[0];
     }
 };
