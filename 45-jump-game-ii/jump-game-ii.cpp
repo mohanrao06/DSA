275fbbaec182e41f26d72b91fffd61a406849fc9
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-02-19 09:46:15 +0530

    Time: 53 ms (22.96%) | Memory: 21.7 MB (16.23%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -16,7 +16,19 @@ public:
     }
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return dfs(nums,0,dp);
+        if(n <= 1) return 0;
+        vector<int> dp(n,INT_MAX);
+        
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            int mini=INT_MAX;
+            for(int j=1;j<=nums[i] && i+j<n;j++){
+                if(dp[i+j]!=INT_MAX){
+                    mini=min(dp[i+j]+1,mini);
+                }
+            }
+            dp[i]=mini;
+        }
+        return dp[0];
     }
 };
