8f38df12aaf6c1e9196bab3fdce1d5aa7500b94d
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-04-30 17:51:03 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (81.61%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,34 +1,12 @@
 class Solution {
 public:
-    int dfs(vector<int>& nums,int ind,vector<int> &dp){
-        if(ind==nums.size()-1)return 1;
-        if(ind>=nums.size())return 0;
-        if(dp[ind]!=-1)return dp[ind];
-        for(int i=1;i<=nums[ind];i++){
-            if(dfs(nums,ind+i,dp)){
-                return dp[ind]=1;
-            }
-        }
-        return dp[ind]=0;
-    }
     bool canJump(vector<int>& nums) {
+        int maxi=0;
         int n=nums.size();
-        vector<int> dp(n,0);
-        for(int i=n-1;i>=0;i--){
-            if(i==n-1){
-                dp[i]=1;
-                continue;
-            }
-            for(int j=1;j<=nums[i];j++){
-                if(i+j<n){
-                    if(dp[i+j]){
-                        dp[i]=1;
-                        break;
-                    }
-                }
-            }
-           
+        for(int i=0;i<n;i++){
+            if(i>maxi)return false;
+            maxi=max(maxi,nums[i]+i);
         }
-        return dp[0];
+        return true;
     }
 };
