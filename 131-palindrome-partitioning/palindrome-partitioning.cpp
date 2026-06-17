4c427ae5949dda6b46b08b5e8ab568acf3f8bf23
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-06-17 17:26:34 +0530

    Time: 64 ms (24.13%) | Memory: 61.8 MB (39.25%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void fun(int ind,string s,vector<vector<string>> &ans,vector<string> &temp){
+    void fun(int ind,string s,vector<string> &temp,vector<vector<string>> &ans){
         if(ind==s.size()){
             ans.push_back(temp);
             return;
@@ -8,16 +8,15 @@ public:
         for(int i=ind;i<s.size();i++){
             if(palindrome(s,ind,i)){
                 temp.push_back(s.substr(ind,i-ind+1));
-                fun(i+1,s,ans,temp);
+                fun(i+1,s,temp,ans);
                 temp.pop_back();
             }
-
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> temp;
-        fun(0,s,ans,temp);
+        fun(0,s,temp,ans);
         return ans;
     }
     bool  palindrome(string s,int start,int end){
