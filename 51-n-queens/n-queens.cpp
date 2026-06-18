3e1ef32a1547bd6a88fdebf8af4a55317eb10027
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-06-18 20:57:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (59.14%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
-    void fun(int col,vector<string> &board, vector<vector<string>> &ans,int n){
-        if(col==n){
+    void fun(int row,vector<string> &board,vector<vector<string>> &ans,int n){
+        if(row==n){
             ans.push_back(board);
+            return;
         }
-        for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
-                board[row][col]='Q';
-                fun(col+1,board,ans,n);
-                board[row][col]='.';
+
+        for(int c=0;c<n;c++){
+            if(isVaild(row,c,board,n)){
+                board[row][c]='Q';
+                fun(row+1,board,ans,n);
+                board[row][c]='.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> board(n);
         string s(n,'.');
+        vector<string> board(n);
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-
         fun(0,board,ans,n);
         return ans;
-
     }
-    bool isSafe(int r,int c,vector<string> board,int n){
-        int drow=r;
-        int dcol=c;
-        while(r>=0 && c>=0){
-            if(board[r][c]=='Q')return false;
-            r--;
-            c--;
+    bool isVaild(int r,int c,vector<string> &board,int n){
+        int row=r;
+        int col=c;
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q')return false;
+            row--;
+            col--;
         }
-        r=drow;
-        c=dcol;
-        while(c>=0){
-            if(board[r][c]=='Q')return false;
-            c--;
+        row = r;
+        col = c;
+
+        while(row >= 0 && col < board.size()) {
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col++;
         }
-        r=drow;
-        c=dcol;
-        while(r<n && c>=0){
-            if(board[r][c]=='Q')return false;
-            c--;
-            r++;
+        row=r;
+        col=c;
+         while(row>=0){
+            if(board[row][col]=='Q')return false;
+            row--;
         }
         return true;
 
