1fd139d6321bf9c3961a8d7aa90c152329e04cbf
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-06-27 17:29:04 +0530

    Time: 16 ms (80.01%) | Memory: 85.9 MB (20.23%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& height) {
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        vector<int> PSE(n,-1);
+        vector<int> NSE(n,n);
         stack<int> st;
-        int n = height.size();
-
-        vector<int> NSE(n, n);
-        vector<int> PSE(n, -1);
-
-        // Next Greater Element (Right)
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && height[i] <= height[st.top()]) {
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
                 st.pop();
             }
-            NSE[i] = !st.empty() ? st.top() : n;
+            PSE[i]=!st.empty()?st.top():-1;
             st.push(i);
         }
-
-        // Clear stack
         st = stack<int>();
-
-        // Previous Greater Element (Left)
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && height[i] <= height[st.top()]) {
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()]>= heights[i]){
                 st.pop();
             }
-            PSE[i] = !st.empty() ? st.top() : -1;
+            NSE[i]=!st.empty()?st.top():n;
             st.push(i);
         }
-
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            ans =max(ans,height[i]*(NSE[i]-PSE[i]-1));
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,heights[i]*(NSE[i]-PSE[i]-1));
         }
-
         return ans;
+        
     }
 };
