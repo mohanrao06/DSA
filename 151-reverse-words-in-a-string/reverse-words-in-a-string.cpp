2f9d4857642b2bebaa7f3ce039b8ba45a44ddb3d
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-06-29 11:04:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.33%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -4,14 +4,16 @@ public:
         string temp=s;
         reverse(temp.begin(),temp.end());
         string ans="";
-        for(int i=0;i<temp.size();i++){
+        int n=s.size();
+        for(int i=0;i<n;i++){
             string word="";
-            while(i<temp.size() && temp[i]!=' '){
+            while(i<n && temp[i]!=' '){
                 word+=temp[i];
                 i++;
             }
             reverse(word.begin(),word.end());
-            if(word.length()>0)ans+=" "+word;
+            if(word.size()>0)ans+=" "+word;
+
 
         }
         return ans.substr(1);
