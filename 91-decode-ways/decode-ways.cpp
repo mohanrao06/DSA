f1c1bbc95951ceb5ebb60a72bb270787cb3a7048
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-06-30 12:25:49 +0530

    Time: 2 ms (22.61%) | Memory: 11.3 MB (6.78%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int fun(int ind,string s,vector<int> &dp){
-        if(s.size()==ind)return 1;
+        if(ind==s.size())return 1;
         if(s[ind]=='0')return 0;
         if(dp[ind]!=-1)return dp[ind];
-        int one=fun(ind+1,s,dp);
-        int two=0;
+        int oneDigit=fun(ind+1,s,dp);
+        int towDigit=0;
         if(ind+1<s.size()){
             int sub=stoi(s.substr(ind,2));
             if(sub>=10 && sub<=26){
-                two=fun(ind+2,s,dp);
+                towDigit=fun(ind+2,s,dp);
             }
-
         }
-        
-        return dp[ind] = one+two;
-
+        return dp[ind]=oneDigit+towDigit;
     }
     int numDecodings(string s) {
+
         int n=s.size();
         vector<int> dp(n,-1);
         return fun(0,s,dp);
