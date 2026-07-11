7aec0bb8b5ce5088507250a0dd2662a12c8b8d8d
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-07-11 23:00:42 +0530

    Time: 76 ms (20.99%) | Memory: 131.5 MB (55.38%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,33 +1,68 @@
 class Solution {
 public:
-    void dfs(vector<int>adj[],int ind,vector<int> &vis,int &node,int &ed){
-        vis[ind]=1;
-        node++;
-        ed+=adj[ind].size();
-        for(auto it:adj[ind]){
-            if(!vis[it]){
-                dfs(adj,it,vis,node,ed);
-            }
-        }
-    }
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<int>adj[n];
-        for(int i=0;i<edges.size();i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        int cnt=0;
-        vector<int> vis(n,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                int ed=0,node=0;
-                dfs(adj,i,vis,node,ed);
-                if(ed==node*(node-1))cnt++;
-            }
-        }
-        return cnt;
+    class DSU{
+        public:
+        vector<int> parent;
+        vector<int> size;
 
+        DSU(int n){
+            parent.resize(n);
+            size.resize(n);
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+                size[i]=1;
+            }
+        }
+        int find(int x){
+            if(parent[x]==x)return x;
+            return parent[x]=find(parent[x]);
+        }
+        void Union(int u,int v){
+            int up=find(u);
+            int vp=find(v);
+            if(up==vp){
+                return;
+            }
+
+            if(size[up]>size[vp]){
+                parent[vp]=up;
+                size[up]+=size[vp];
+            }else if(size[up]<size[vp]){
+                parent[up]=vp;
+                size[vp]+=size[up];
+            }else{
+                parent[vp]=up;
+                size[up]+=size[vp];
+            }
+        }
+
+
+
+    };
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        DSU dsu(n);
+        unordered_map<int,int> mp;
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+
+            dsu.Union(u,v);
+        }
+
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            int root=dsu.find(u);
+            mp[root]++;
+        }
+        int res=0;
+        for(int i=0;i<n;i++){
+            if(dsu.find(i)==i){
+                int v=dsu.size[i];
+                int e=mp[i];
+                if((v*(v-1))/2==e)res++;
+            }
+        }
+        return res;
     }
 };
