b8f0a38ee26f30b64322d63957419e7df424593a
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-07-22 23:00:05 +0530

    Time: 233 ms (5.08%) | Memory: 22.9 MB (6.26%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    
-    int dfs(vector<int>& nums,int ind,vector<int> &dp){
+    int fun(int ind,vector<int>& nums,vector<int> &dp){
         if(ind==nums.size()-1)return 0;
-        if(ind>=nums.size())return INT_MAX;
         if(dp[ind]!=-1)return dp[ind];
-        int mini=INT_MAX;
+        int cnt=1e9;
         for(int i=1;i<=nums[ind];i++){
-            int ans=dfs(nums,ind+i,dp);
-            if(ans!=INT_MAX)
-            mini=min(mini,ans+1);
+            if(ind+i<nums.size()){
+                cnt=min(cnt,1+fun(ind+i,nums,dp));
+            }
         }
-        return dp[ind]=mini;
-
+        return dp[ind]=cnt;
     }
     int jump(vector<int>& nums) {
         int n=nums.size();
-        if(n <= 1) return 0;
-        vector<int> dp(n,INT_MAX);
-        
-        dp[n-1]=0;
-        for(int i=n-2;i>=0;i--){
-            int mini=INT_MAX;
-            for(int j=1;j<=nums[i] && i+j<n;j++){
-                if(dp[i+j]!=INT_MAX){
-                    mini=min(dp[i+j]+1,mini);
-                }
-            }
-            dp[i]=mini;
-        }
-        return dp[0];
+        vector<int> dp(n,-1);
+
+
+        return fun(0,nums,dp);
+
     }
 };
