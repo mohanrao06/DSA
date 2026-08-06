fec2963c716ac6bf063549707553ccfd729f9cc3
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-08-06 14:52:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (24.33%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -14,7 +14,13 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return fun(0,nums,dp);
+        if(n==1)return nums[0];
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
     }
 };
