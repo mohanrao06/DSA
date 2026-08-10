636e00feb6a683d4d1ad8938b553dbb3ac9a22b7
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-08-10 12:35:26 +0530

    Time: 43 ms (57.44%) | Memory: 18.2 MB (29.16%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    bool fun(int n, vector<int>& dp) {
+
+        if (n == 0)
+            return false;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        for (int i = 1; i * i <= n; i++) {
+
+            if (!fun(n - i * i, dp)) {
+                return dp[n] = true;
+            }
+        }
+
+        return dp[n] = false;
+    }
+
+    bool winnerSquareGame(int n) {
+
+        vector<int> dp(n + 1, -1);
+
+        return fun(n, dp);
+    }
+};
