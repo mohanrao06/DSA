ff2cc9e53bbe8112a7d27f067065f707d55849fb
G mohan rao <150166791+mohanrao06@users.noreply.github.com>
2026-08-25 13:44:15 +0530

    Time: 45 ms (27.09%) | Memory: 37.1 MB (21.67%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-    int fun(int ind,int t,vector<int> &arr,vector<vector<int>> &dp){
-        if(ind==0){
-            if(t%arr[ind]==0)return 1;
-            else{
-                return 0;
-            }
-        }
-        if(dp[ind][t]!=-1)return dp[ind][t];
-        int nottake=fun(ind-1,t,arr,dp);
-        int take=0;
-        if(arr[ind]<=t){
-            take=fun(ind,t-arr[ind],arr,dp);
-        }
-        return dp[ind][t]=nottake+take;
+    int fun(int ind,int amount, vector<int>& coins,vector<vector<int>> &dp){
+        if(amount==0)return 1;
+        if(ind>=coins.size())return 0;
+        if(dp[ind][amount]!=-1)return dp[ind][amount];
 
+        int nottake=fun(ind+1,amount,coins,dp);
+        int take=0;
+        if(amount>=coins[ind]){
+            take=fun(ind,amount-coins[ind],coins,dp);
+        }
+        return dp[ind][amount]=take+nottake;
     }
-    int change(int amount, vector<int>& arr) {
-        int n=arr.size();
-        vector<vector<unsigned long long>> dp(n,vector<unsigned long long> (amount+1,0));
-        for(int i=0;i<=amount;i++){
-            if(i%arr[0]==0){
-                dp[0][i]=1;
-            }else{
-                dp[0][i]=0;
-            }
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                unsigned long long nottake=dp[i-1][j];
-                unsigned long long take=0;
-                if(arr[i]<=j){
-                    take=dp[i][j-arr[i]];
-                }
-                dp[i][j]=take+nottake;
-            }
-        }
-        return (int)dp[n-1][amount];
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        return fun(0,amount,coins,dp);
     }
 };
